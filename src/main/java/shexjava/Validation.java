063593dff52bf9d7cd3a4d777b31960dc03a165a
rgmprabodha <rgmprabodha@gmail.com>
2020-04-10 10:27:29 +0200

    Validation result write to file

--- src/main/java/shexjava/Validation.java
+++ src/main/java/shexjava/Validation.java
@@ -1,6 +1,6 @@
 package shexjava;
 
-
+import java.io.ByteArrayInputStream;
 import java.io.FileWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import org.apache.commons.rdf.api.Graph;
 import org.apache.commons.rdf.api.IRI;
 import org.apache.commons.rdf.rdf4j.RDF4J;
+import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.util.FileManager;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.rio.RDFFormat;
@@ -17,6 +18,10 @@ import fr.inria.lille.shexjava.GlobalFactory;
 import fr.inria.lille.shexjava.schema.Label;
 import fr.inria.lille.shexjava.schema.ShexSchema;
 import fr.inria.lille.shexjava.schema.parsing.GenParser;
+import fr.inria.lille.shexjava.shapeMap.BaseShapeMap;
+import fr.inria.lille.shexjava.shapeMap.ResultShapeMap;
+import fr.inria.lille.shexjava.shapeMap.parsing.ShapeMapParsing;
+import fr.inria.lille.shexjava.validation.RecursiveValidationWithMemorization;
 import fr.inria.lille.shexjava.validation.RefineValidation;
 import fr.inria.lille.shexjava.validation.ValidationAlgorithm;
 
@@ -25,56 +30,70 @@ public class Validation {
 	public static void main(String[] args) throws Exception {
 
 		Path path = Paths.get(".").toAbsolutePath().normalize();
+		ShapeMapParsing parser = new ShapeMapParsing();
 
-		String dataf = path.toFile().getAbsolutePath() + "/src/main/resources/temperature.ttl";
-		Path shape = Paths.get("./src/main/resources/shape.json").toAbsolutePath().normalize();
+		String DATA = path.toFile().getAbsolutePath() + "/src/main/resources/temperature.ttl";
+		Path SHAPE = Paths.get("./src/main/resources/shape.json").toAbsolutePath().normalize();
 
 		RDF4J factory = new RDF4J();
 		GlobalFactory.RDFFactory = factory; // set the global factory used in shexjava
 
 		// load and create the shex schema
-		ShexSchema schema = GenParser.parseSchema(shape);
-		
-//		System.out.println("Reading schema");
-//		for (Label label : schema.getRules().keySet())
-//			System.out.println(label + ": " + schema.getRules().get(label));
+		ShexSchema schema = GenParser.parseSchema(SHAPE);
 
-		// load the model
+		// load the data to model
 		String baseIRI = "http://a.example.shex/";
-		Model data = Rio.parse(FileManager.get().open(dataf), baseIRI, RDFFormat.TURTLE);
-//		System.out.println("Reading data");
-//		Iterator<Statement> ite = data.iterator();
-//		while (ite.hasNext())
-//			System.out.println(ite.next());
-
-		// create the graph
+		Model data = Rio.parse(FileManager.get().open(DATA), baseIRI, RDFFormat.TURTLE);
+		
+		// create the data graph
 		Graph dataGraph = factory.asGraph(data);
 
 		// focus node and shape label to be validated
-		IRI focusNode = factory.createIRI("http://example.com/ns#Sensor1"); 
-		Label shapeLabel = new Label(factory.createIRI("http://example.com/ns#TemperatureShape")); 
+//		IRI focusNode = factory.createIRI("http://example.com/ns#Sensor1");
+//		Label shapeLabel = new Label(factory.createIRI("http://example.com/ns#TemperatureShape"));
+		
+		 // load the shape map
+        String shMap = "{FOCUS a <http://example.com/ns#Sensor>}@<http://example.com/ns#TemperatureShape>";
 
+        try {
+            BaseShapeMap shapeMap = parser.parse(new ByteArrayInputStream(shMap.getBytes()));
+//            System.out.println(shapeMap);
+            ValidationAlgorithm vl = new RefineValidation(schema, dataGraph);
+			
+			ResultShapeMap resultaa = vl.validate(shapeMap);
+//            System.out.println(resultaa);
+            
+            RecursiveValidationWithMemorization algo = new RecursiveValidationWithMemorization(schema, dataGraph);
+            ResultShapeMap result = algo.validate(shapeMap);
+            System.out.println(result.getAssociations().toString());
+//            System.out.println(result);
+            
+            String reportFile = path.toFile().getAbsolutePath() + "/src/main/resources/temperatureReport.ttl";
+
+    		FileWriter myWriter = new FileWriter(reportFile);
+    		myWriter.write(result.toString());
+    		myWriter.close();
+
+        } catch ( Exception e) {
+            e.printStackTrace();
+        }
 		System.out.println();
 		System.out.println("Refine validation:");
 		// create the validation algorithm
 		ValidationAlgorithm validation = new RefineValidation(schema, dataGraph);
 
+		
+
 		// validate
-		validation.validate(focusNode, shapeLabel);
-		System.out.println(validation.getTyping().getStatusMap());
+//		validation.validate(shapeMap);
+		System.out.println(validation.getTyping());
 
-		String reportFile = path.toFile().getAbsolutePath() + "/src/main/resources/temperatureReport.ttl";
-
-		FileWriter myWriter = new FileWriter(reportFile);
-		myWriter.write(validation.getTyping().getStatusMap().toString());
-		myWriter.close();
-
+		
 		System.out.println("Done");
 
 		// check the result
-		boolean result = validation.getTyping().isConformant(focusNode, shapeLabel);
-		System.out.println("Does " + focusNode + " has shape " + shapeLabel + "? " + result);
-
+//		boolean result = validation.getTyping().isConformant(focusNode, shapeLabel);
+//		System.out.println("Does " + focusNode + " has shape " + shapeLabel + "? " + result);
 
 	}
 }
